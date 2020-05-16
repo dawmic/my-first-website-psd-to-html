cf06255d489b11b1f0f89bf48e60deedfe6d763c
Dawid M <dawmic19@gmail.com>
2020-05-16 15:35:45 +0200

    remove unnecessary comments

--- script.js
+++ script.js
@@ -1,16 +1,3 @@
-/* ??????????????
-let btn = document.querySelector('.btn');
-let lista = document.querySelector('ul');
-
-btn.addEventListener('click', ja);
-
-function ja() {
-    if (lista.className == 'ukryj')
-        lista.classList.remove('ukryj');
-    else
-        lista.classList.add('ukryj');
-}
-*/
 "use strict";
 
 const button = document.querySelector('.menu-btn');
