fc32815fcc3bfe5deafd9a12cd0c96912f5d4469
Dawid M <dawmic19@gmail.com>
2020-05-17 12:01:29 +0200

    hamburger menu

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ const button = document.querySelector('.menu-btn');
 button.addEventListener('click', function() {
     document.querySelector('.menu').classList.toggle('menu-mobile');
 });
+
+const icons = document.querySelectorAll('.icon');
+icons.forEach(icon => {
+    icon.addEventListener('click', (event) => {
+        icon.classList.toggle("open");
+    });
+});
