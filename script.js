8ac7189ebefb30ca36f3ae2b590e4eb083532f68
Dawid M <dawmic19@gmail.com>
2020-06-26 09:02:10 +0200

    sticky menu

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 "use strict";
-
+const menu_mobile = document.querySelector('.menu-mobile');
 const button = document.querySelector('.menu-btn');
 button.addEventListener('click', function() {
     document.querySelector('.menu').classList.toggle('menu-mobile');
+
+
 });
 
 const icons = document.querySelectorAll('.icon');
@@ -11,3 +13,17 @@ icons.forEach(icon => {
         icon.classList.toggle("open");
     });
 });
+
+const secTeam = document.querySelector('.sec-team');
+const navi = document.querySelector('.navigation');
+
+const menuLinks = document.querySelectorAll('.menuLink');
+
+menuLinks.forEach(x => {
+    x.addEventListener('click', () => {
+        console.log('click');
+        document.querySelector('.menu').classList.remove('menu-mobile');
+        document.querySelector('.icon').classList.remove('open');
+
+    })
+})
